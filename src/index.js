fd091d9cb72b6e511ccfaab5fef1c3a773decfc1
Pavel Emelyanov <pavel_emel98@mail.ru>
2022-09-29 17:51:17 +0300

    task TREESUM

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import { treeSum } from "./assets/components/TREESUM/TREESUM";
 
 const root = document.querySelector('#root');
 
-root.insertAdjacentHTML('beforeend', `<div> ${treeSum([ 5, 7, 
-  [ 4, [2], 8, [1,3], 2 ], 
-  [ 9, [] ], 
-  1, 8
-])} </div>`)
+root.insertAdjacentHTML('beforeend', `<div> ${treeSum([ 5, 7, [ 4, [2], 8, [1,3], 2 ],[ 9, [] ], 1, 8 ])} </div>`)
+
+  
+  
+  
 
